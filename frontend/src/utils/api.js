6eed327bcc3a2a490a145f8c422c79ae0ce49dfc
Noor jahan <noorifms@gmail.com>
2025-08-25 16:34:54 +0530

    medplus

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://quickmed-logistics-and-transportation.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
